954cf00dbfa44484cf1345a425251737e2e6ccd9
Jay Chakravarty <jaychakravarty@icloud.com>
2025-05-14 14:38:16 -0700

    update connection url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const PORT = 3000;
 
 // Connect to the DB
-mongoose.connect(/*process.env.MONGO_CONNECT_URI*/'mongodb://127.0.0.1:27017/final-project').then(() => {
+mongoose.connect(process.env.MONGO_CONNECT_URI).then(() => {
     console.log('DB connected!');
 
     // Start the server
