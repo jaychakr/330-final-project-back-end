5c3e98192e08783a7f8af2694c404df95ddeabf1
Jay Chakravarty <jaychakravarty@icloud.com>
2025-05-21 13:42:16 -0700

    Sort by updatedAt descending

--- daos/conversation.js
+++ daos/conversation.js
@@ -10,6 +10,6 @@ module.exports.create = async (userId1, userId2) => {
 module.exports.findAllById = async (userId) => {
     const conversations = await Conversation.find({
         $or: [{ userId1: userId }, { userId2: userId }]
-    });
+    }).sort({ updatedAt: -1 });
     return conversations;
 }
